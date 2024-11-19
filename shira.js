95e952306a51020d118b0516a24c0f157f424c6a
arasu003 <166005507+arasu003@users.noreply.github.com>
2024-11-19 00:21:11 -0800

    Add files via upload

---
+++ shira.js
@@ -0,0 +1,11 @@
+// Function to add item to cart
+function addToCart(productName) {
+    alert(`${productName} has been added to the cart.`);
+  }
+  
+  // Function to search products
+  function search() {
+    const query = document.getElementById('search-bar').value;
+    alert(`You searched for: ${query}`);
+  }
+  
